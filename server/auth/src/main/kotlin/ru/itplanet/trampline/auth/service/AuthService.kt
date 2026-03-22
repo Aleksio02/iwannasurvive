1f66661dd93515d47ec27aca4044cff821bc4a73
AlekseyIshchenko <oleks1o@yandex.ru>
2026-03-22 23:23:20 +0300

    feat/tramplin-003 Исправил замечания: - Создал сущность под форму регистрации и исправил код исходя из этого

--- server/auth/src/main/kotlin/ru/itplanet/trampline/auth/service/AuthService.kt
+++ server/auth/src/main/kotlin/ru/itplanet/trampline/auth/service/AuthService.kt
@@ -2,10 +2,11 @@ package ru.itplanet.trampline.auth.service
 
 import ru.itplanet.trampline.auth.model.TokenPayload
 import ru.itplanet.trampline.auth.model.request.Authorization
+import ru.itplanet.trampline.auth.model.request.Registration
 import ru.itplanet.trampline.auth.model.response.AuthResponse
 
 interface AuthService {
-    fun register(request: Authorization): AuthResponse
+    fun register(request: Registration): AuthResponse
     fun login(request: Authorization): AuthResponse
     fun validateSession(sessionId: String?): TokenPayload
 }
