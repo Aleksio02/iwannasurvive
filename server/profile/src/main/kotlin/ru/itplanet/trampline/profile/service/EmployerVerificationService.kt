56d7da44570f81ac742b5f4b0cef6708547591c5
PrimakMV <pm2710@mail.ru>
2026-03-29 01:33:01 +0300

    feat(tramplin-054): add moderation service integration to profile

--- server/profile/src/main/kotlin/ru/itplanet/trampline/profile/service/EmployerVerificationService.kt
+++ server/profile/src/main/kotlin/ru/itplanet/trampline/profile/service/EmployerVerificationService.kt
@@ -2,14 +2,26 @@ package ru.itplanet.trampline.profile.service
 
 import org.springframework.web.multipart.MultipartFile
 import ru.itplanet.trampline.commons.model.file.InternalFileAttachmentResponse
+import ru.itplanet.trampline.commons.model.moderation.InternalModerationTaskLookupResponse
 import ru.itplanet.trampline.profile.model.request.EmployerVerificationRequest
 import ru.itplanet.trampline.profile.model.response.EmployerVerificationResponse
 
 interface EmployerVerificationService {
+
     fun createVerificationRequest(
         employerUserId: Long,
         request: EmployerVerificationRequest,
     ): EmployerVerificationResponse
+
+    fun getModerationTask(
+        employerUserId: Long,
+        verificationId: Long,
+    ): InternalModerationTaskLookupResponse
+
+    fun cancelModerationTask(
+        employerUserId: Long,
+        verificationId: Long,
+    )
 
     fun addAttachment(
         employerUserId: Long,
