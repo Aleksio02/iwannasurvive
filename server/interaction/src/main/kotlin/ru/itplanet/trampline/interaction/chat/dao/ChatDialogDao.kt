7e7dcbe06a125e0510b996a46f9cbd5513ad9f13
PrimakMV <pm2710@mail.ru>
2026-04-04 00:04:13 +0300

    feat(tramplin-089): add models and migration db

---
+++ server/interaction/src/main/kotlin/ru/itplanet/trampline/interaction/chat/dao/ChatDialogDao.kt
@@ -0,0 +1,13 @@
+package ru.itplanet.trampline.interaction.chat.dao
+
+import org.springframework.data.jpa.repository.JpaRepository
+import ru.itplanet.trampline.interaction.chat.dao.dto.ChatDialogDto
+
+interface ChatDialogDao : JpaRepository<ChatDialogDto, Long> {
+    fun findByOpportunityResponseId(opportunityResponseId: Long): ChatDialogDto?
+
+    fun findByApplicantUserIdOrEmployerUserIdOrderByLastMessageAtDescIdDesc(
+        applicantUserId: Long,
+        employerUserId: Long,
+    ): List<ChatDialogDto>
+}
